47146e9caf7d7d1f4adebd4f443d934403dad88f
Lutonet <101110383+Lutonet@users.noreply.github.com>
2022-06-19 23:05:43 +0100

    ted

--- Services/IFileService.cs
+++ Services/IFileService.cs
@@ -3,7 +3,9 @@
     public interface IFileService
     {
         Task<bool> CheckIfDefaultExists(string folder, string defaultLanguage);
+
         Task<bool> CheckIfFileExists(string folder, string languageId);
+
         Task<bool> CheckIfOldExists(string folder);
 
         Task<Dictionary<string, string>> GetLanguage(string folder, string languageCode);
